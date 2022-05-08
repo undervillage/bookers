db1f0727b9cb6bac15aca0e3a9249d3e226f216a
undervillage <undervillage.hjm@gmail.com>
2022-05-08 20:02:36 +0900

    修正

--- Rails6-bookers2-debug-main/app/controllers/books_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/books_controller.rb
@@ -50,7 +50,7 @@ class BooksController < ApplicationController
   def ensure_correct_user
     book = Book.find(params[:id])
     unless book.user == current_user
-      redirect_to user_path(current_user)
+      redirect_to books_path
     end
   end
 end
